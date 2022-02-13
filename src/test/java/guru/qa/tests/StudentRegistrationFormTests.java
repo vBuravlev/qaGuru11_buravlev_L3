290a7a565278d419aa3633517cb4cc6a676c3ee2
User <masta54@mail.ru>
2022-02-13 11:15:33 +0700

    Test RegistrationPage (PageObject)

--- src/test/java/guru/qa/tests/StudentRegistrationFormTests.java
+++ src/test/java/guru/qa/tests/StudentRegistrationFormTests.java
@@ -2,20 +2,17 @@ package guru.qa.tests;
 
 
 import com.codeborne.selenide.Configuration;
+import guru.qa.pages.RegistrationPage;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
-
 
 public class StudentRegistrationFormTests {
+    //import
+    private RegistrationPage registrationPage = new RegistrationPage();
 
     //configuration parameters
-    static String urlStudentRegistrationForm = "https://demoqa.com/automation-practice-form";
+    static String urlStudentRegistrationForm = "https://demoqa.com";
     static String sizeBrowser = "1920x1080";
     static String firstName = "StudentFirstName";
     static String lastName = "StudentLastName";
@@ -24,12 +21,14 @@ public class StudentRegistrationFormTests {
     static String subjectsFullText = "Maths";
     static String subjectsShortText = "Hi";
     static String subjectsShortTextSelect = "History";
-    static String calendarYear = "2023"; // "yyyy" format
-    static String calendarMonth = "May"; // number of characters = 3
-    static String calendarDay = "10";  // "dd" format
+    static String calendarYear = "1987"; // "yyyy" format
+    static String calendarMonth = "November"; // number of characters = 3
+    static String calendarDay = "14";  // "dd" format
     static String genderRoleName = "Male"; //"Male", "Female", "Other"
+    static String path = "src/test/resources/";
     static String fileName = "History.PNG";
-    static String currentAddress = "Moscow";
+    static String currentAddress = "Novosibirsk";
+    static String textModalWindow = "Thanks for submitting the form";
     String[] hobbiesUser = new String[]{"Reading", "Sports", "Music"};  //"Reading", "Sports", "Music"
     static String stateName = "NCR";
     static String cityName = "Delhi";
@@ -37,79 +36,44 @@ public class StudentRegistrationFormTests {
 
     @BeforeAll
     static void beforeAll() {
+        Configuration.baseUrl = urlStudentRegistrationForm;
         Configuration.browserSize = sizeBrowser;
     }
 
     @Test
     void successTest() {
-        open(urlStudentRegistrationForm);
 
-        $("#firstName").setValue(firstName);
-        $("#lastName").setValue(lastName);
-        $("#userEmail").setValue(userEmail);
-
-        $x("//label[text()='" + genderRoleName + "']").click();
-
-        $("#userNumber").setValue(userNumber);
-
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption(calendarMonth);
-        $(".react-datepicker__year-select").selectOption(calendarYear);
-        $(".react-datepicker__day--0" + calendarDay).click();
-
-        $("#subjectsInput").click();
-        $("#subjectsInput").setValue(subjectsFullText).pressEnter();
-        $("#subjectsInput").click();
-        $("#subjectsInput").setValue(subjectsShortText);
-        $(byText(subjectsShortTextSelect)).click();
-
-        $("#uploadPicture").uploadFile(new File("src/test/resources/" + fileName));
-
-        for (String user : hobbiesUser) {
-            $x("//label[text()='" + user + "']").click();
-        }
-
-        $("#currentAddress").setValue(currentAddress);
-
-        $("#state").click();
-        $(byText(stateName)).click();
-
-        $("#city").click();
-        $(byText(cityName)).click();
-
-        $("#submit").click();
-
+        //input data in form
+        registrationPage.openPage()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setGender(genderRoleName)
+                .setEmailName(userEmail)
+                .setUserNumber(userNumber)
+                .setCurrentAddress(currentAddress)
+                .setHobbiesUser(hobbiesUser)
+                .uploadPicture(path, fileName)
+                .setSubjectsFull(subjectsFullText)
+                .setSubjectsShort(subjectsShortText, subjectsShortTextSelect)
+                .selectState(stateName)
+                .selectCity(cityName)
+                .setBirthDate(calendarDay, calendarMonth, calendarYear)
+                .openResultDataForm(textModalWindow);
 
         //checking the results in the registration form
-        $x("//td[contains(.,'Student Name')]/following-sibling::td").shouldHave(text(firstName + " " + lastName));
-        $x("//td[contains(.,'Student Email')]/following-sibling::td").shouldHave(text(userEmail));
-        $x("//td[contains(.,'Gender')]/following-sibling::td").shouldHave(text(genderRoleName));
-        $x("//td[contains(.,'Mobile')]/following-sibling::td").shouldHave(text(userNumber));
-        $x("//td[contains(.,'Date of Birth')]/following-sibling::td").shouldHave(text(calendarDay + " " + calendarMonth + "," + calendarYear));
-        $x("//td[contains(.,'Subjects')]/following-sibling::td").shouldHave(text(subjectsFullText));
-        $x("//td[contains(.,'Subjects')]/following-sibling::td").shouldHave(text(subjectsShortTextSelect));
-
-        for (String user : hobbiesUser) {
-            $x("//td[contains(.,'Hobbies')]/following-sibling::td").shouldHave(text(user));
-        }
-
-        $x("//td[contains(.,'Picture')]/following-sibling::td").shouldHave(text(fileName));
-        $x("//td[contains(.,'Address')]/following-sibling::td").shouldHave(text(currentAddress));
-        $x("//td[contains(.,'State and City')]/following-sibling::td").shouldHave(text(stateName + " " + cityName));
-
-        $("#closeLargeModal").click();
-        $("#closeLargeModal").shouldNotBe(visible);
-
-        //registration form cleanup check
-        $("#firstName").setValue("");
-        $("#lastName").setValue("");
-        $("#userEmail").setValue("");
-        $("#userNumber").setValue("");
-        $("#subjectsInput").setValue("");
-        $("#currentAddress").setValue("");
-
-        $("#submit").click();
-        $("#closeLargeModal").shouldNotBe(visible);
+        registrationPage
+                .checkForm("Student Name", firstName + " " + lastName)
+                .checkForm("Student Email", userEmail)
+                .checkForm("Gender", genderRoleName)
+                .checkForm("Mobile", userNumber)
+                .checkForm("Date of Birth", calendarDay + " " + calendarMonth + "," + calendarYear)
+                .checkForm("Subjects", subjectsFullText)
+                .checkForm("Subjects", subjectsShortTextSelect)
+                .checkFormIteration("Hobbies", hobbiesUser)
+                .checkForm("Picture", fileName)
+                .checkForm("Address", currentAddress)
+                .checkForm("State and City", stateName + " " + cityName)
+                .closeResultDataForm();
 
     }
 }
