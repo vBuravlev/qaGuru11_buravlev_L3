6a98ab469c33a9da00273e86cad85c65c797c4e6
User <masta54@mail.ru>
2022-02-15 08:32:32 +0700

    TestDate

---
+++ src/test/java/guru/qa/tests/TestData.java
@@ -0,0 +1,25 @@
+package guru.qa.tests;
+
+public class TestData {
+
+    //configuration parameters
+    public static String
+            firstName = "StudentFirstName",
+            lastName = "StudentLastName",
+            userEmail = "email@email.com",
+            userNumber = "8800123456",
+            subjectsFullText = "Maths",
+            subjectsShortText = "Hi",
+            subjectsShortTextSelect = "History",
+            calendarYear = "1987",
+            calendarMonth = "November",
+            calendarDay = "14",
+            genderRoleName = "Male",
+            path = "src/test/resources/",
+            fileName = "History.PNG",
+            currentAddress = "Novosibirsk",
+            textModalWindow = "Thanks for submitting the form",
+            stateName = "NCR",
+            cityName = "Delhi";
+    public static String[] hobbiesUser = new String[]{"Reading", "Sports", "Music"};
+}
