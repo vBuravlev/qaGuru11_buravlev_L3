28572fa3e552458fccee984724241755a241c8aa
User <masta54@mail.ru>
2022-02-16 10:05:26 +0700

    JavaScript Execution

---
+++ src/test/java/guru/qa/tests/StudentRegistrationFormExecuteJavaScriptTests.java
@@ -0,0 +1,187 @@
+package guru.qa.tests;
+
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.qa.pages.RegistrationPage;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static guru.qa.tests.TestData.*;
+import static java.lang.String.format;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.MapDifference;
+import com.google.common.collect.Maps;
+import org.apache.commons.io.FileUtils;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static java.lang.String.format;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StudentRegistrationFormExecuteJavaScriptTests extends TestBase {
+    //import
+    private RegistrationPage registrationPage = new RegistrationPage();
+
+    Map<String, String> expectedData = new HashMap<String, String>() {{
+        put("Student Name", firstName + " " + lastName);
+        put("Student Email", userNumber);
+        put("Gender", genderRoleName);
+    }};
+
+//    @Test
+//    void successTest() {
+//        //input data in form
+//        registrationPage.openPage()
+//                .setFirstName(firstName)
+//                .setLastName(lastName)
+//                .setGender(genderRoleName)
+//                .setEmailName(userEmail)
+//                .setUserNumber(userNumber)
+//                .setCurrentAddress(currentAddress)
+//                .setHobbiesUser(hobbiesUser)
+//                .uploadPicture(path, fileName)
+//                .setSubjectsFull(subjectsFullText)
+//                .setSubjectsShort(subjectsShortText, subjectsShortTextSelect)
+//                .selectState(stateName)
+//                .selectCity(cityName)
+//                .setBirthDate(calendarDay, calendarMonth, calendarYear)
+//                .openResultDataForm(textModalWindow);
+//
+//        //checking the results in the registration form
+//        registrationPage
+//                .checkForm("Student Name", firstName + " " + lastName)
+//                .checkForm("Student Email", userEmail)
+//                .checkForm("Gender", genderRoleName)
+//                .checkForm("Mobile", userNumber)
+//                .checkForm("Date of Birth", calendarDay + " " + calendarMonth + "," + calendarYear)
+//                .checkForm("Subjects", subjectsFullText)
+//                .checkForm("Subjects", subjectsShortTextSelect)
+//                .checkFormIteration("Hobbies", hobbiesUser)
+//                .checkForm("Picture", fileName)
+//                .checkForm("Address", currentAddress)
+//                .checkForm("State and City", stateName + " " + cityName)
+//                .closeResultDataForm();
+
+    @Test
+    void successfulRegistrationWithSoftAssertsTest() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+        $("#firstName").val(firstName);
+        $("#lastName").val(lastName);
+        $("#userEmail").val(userEmail);
+        $("[name=gender][value=Other]").parent().click();
+        $("#userNumber").val("1231231231");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("July");
+        $(".react-datepicker__year-select").selectOption("2005");
+        $(".react-datepicker__day--028:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").val("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#uploadPicture").uploadFromClasspath("./img/1.png");
+        $("#currentAddress").val("Qa guru street 7");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+
+        Map<String, String> actualData = getTableContentWithExecuteScript();
+
+//        SoftAssertions softly = new SoftAssertions();
+        //assertThat(actualData).isEqualTo(expectedData);
+
+//        for (SelenideElement line : lines) {
+////            String key = line.$("td").text(); // Student Name
+////            String expectedValue = expectedData.get(key);
+////            String actualValue = line.$("td", 1).text();
+////
+////            softly.assertThat(actualValue)
+////                    .as(format("Result in line %s was %s, but expected %s", key, actualValue, expectedValue))
+////                    .isEqualTo(expectedValue);
+//        }
+//        softly.assertAll();
+    }
+
+
+    //        ElementsCollection lines = $$(".table-responsive tbody tr").snapshot();
+//
+//        SoftAssertions softly = new SoftAssertions();
+//
+//        for (SelenideElement line : lines) {
+//            String key = line.$("td").text(); // Student Name
+//            String expectedValue = expectedData.get(key);
+//            String actualValue = line.$("td", 1).text();
+//
+//            softly.assertThat(actualValue)
+//                    .as(format("Result in line %s was %s, but expected %s", key, actualValue, expectedValue))
+//                    .isEqualTo(expectedValue);
+//        }
+//        softly.assertAll();
+//    }
+    public static Map<String, String> getTableContentWithExecuteScript() {
+        String jsCode = readTextFromFile("./src/test/resources/javascript/get_table_data.js");
+        String browserResponse = Selenide.executeJavaScript(jsCode);
+
+
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, String> data = null;
+        try {
+            data = mapper.readValue(browserResponse,
+                    new TypeReference<Map<String, String>>(){});
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+
+        return data;
+    }
+
+    public static String readTextFromFile(String filePath) {
+        String fileContent = null;
+        try {
+            fileContent = FileUtils.readFileToString(new File(filePath), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return fileContent;
+    }
+}
